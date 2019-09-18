a7c6d2e3473171e46fe6a6a33e7de978de3d604a
Chingford <chingford162@outlook.com>
2019-09-18 17:10:14 -0400

    psuedo and code written nothing working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ var playerScore = [];
 
 // Crystal 
 var crystalPink = "";
-var crystalBlue= "";
+var crystalBlue = "";
 var crystalPurple = "";
 var crystalWhite = "";
 
@@ -30,60 +30,65 @@ function newGame() {
   $("#crystalPink").text(crystalPink);
 
   crystalBlue = Math.floor((Math.random() * 12) + 1);
- // $("#crystalBlue").text(crystalBlue);
+  // $("#crystalBlue").text(crystalBlue);
   crystalPurple = Math.floor((Math.random() * 12) + 1);
   //$("#crystalPurple").text(crystalPurple);
 
- crystalWhite = Math.floor((Math.random() * 12) + 1);
+  crystalWhite = Math.floor((Math.random() * 12) + 1);
   //$("#crystalWhite").text(crystalWhite);
 }
 
-//   Create onkeyup event 
-document.onkeyup = function (event) {
-  //on every key press you want to push the user guess into the letters guessed array
-  var userGuess = event.key;
-  // push every leter from the even into our alreadyGuessed array
-  alreadyGuessed.push(userGuess);
-  //grab element to update page
-  //to pass the value of userGuess use the method.text and include the parameter that is passed
-  $("#alreadyGuessed").append(userGuess);
+function playerClick() {
+  //record random number from crystal in the playerScore[]
 
-  //THIS IS YOUR WIN CONDITION!!!
-  // Start if statements for your win;if the user's letter
-  if (userGuess === compGuess.toLowerCase()) {
-    // increase wins by 1
-    wins++;
-    // run reset function you wrote above
+  playerClick.push(playerScore);
+
+  function sum(playerScore) {
+    var i = 0;
+    for (var index = 0; index < playerScore.length; index++) {
+      return index += playerScore[i];
+    }
+  }
+
+
+  /*  for some reason this code though none of the vars are related to the html makes the random numbers generate???
+  document.click = function (event) {
+    //on every key press you want to push the user guess into the letters guessed array
+    var userGuess = event.key;
+    // push every leter from the even into our alreadyGuessed array
+    alreadyGuessed.push(userGuess);
+    //grab element to update page
+    //to pass the value of userGuess use the method.text and include the parameter that is passed
+    $("#alreadyGuessed").append(userGuess);
+*/
+
+    //THIS IS YOUR WIN CONDITION!!!
+    // Start if statements for your win;if the user's letter
+    if (playerScore === displayRandomNum()) {
+      // increase wins by 1
+      wins++;
+      // run reset function you wrote above
+      newGame();
+      // update your HTML here / wins section
+      $("#wins").text(wins);
+    }
+    //add playerScores together
+    else {
+      playerScore--;
+      // update html with userGuessess
+      $("playerScore").text(playerScore);
+    }
+    //THIS IS YOUR LOSS CONDITION
+    // if playerScore exceeds displayRandom number then log a loss
+
+    if (playerScore > displayRandomNum()) {
+      losses++;
+
+      // run reset function you wrote above
+      newGame();
+
+      // update your HTML here / loss section 
+      $("#losses").text(losses);
+    }
+
     newGame();
-    // update your HTML here / wins section
-    $("#wins").text(wins);
-  }
-  // else!
-  //you also want to decrement your guessesLeft variable and update HTML
-  else {
-    userGuesses--;
-    // update html with userGuessess
-    $("#userGuesses").text(userGuesses);
-  }
-
-  //THIS IS YOUR LOSS CONDITION
-  if (userGuesses === 0) {
-    losses++;
-    newGame();
-
-    // update our page with lossess using Jquery
-  }
-  //since you are decrementing the guessesLeft on every keypress your loss condition is here
-
-  //increment losses
-
-  // start a new game using the function you wrote above
-
-  // update your HTML here / wins section
-  $("#losses").text(losses);
-
-}
-
-newGame();
-
-   //run new game function,  you start your newgame function or restart whatever you called it here should be the last thing on the page so every time a user logs in it will start a new game.  Let me know if you need help we can always do a zoom session and i can walk you through if you get stuck
